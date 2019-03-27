a97d02940034aa9173e4b754f0420572b1c75740
Wu <31910557+Sylar5@users.noreply.github.com>
2019-03-27 20:31:42 +0800

    getMiddle.cpp
    Get the middle of two numbers with two methods, (left + right) >> 1 and left + ((right - left) >> 1), and compare these results. As we see, these two methods have the same result.

---
+++ getMiddle.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	int getMiddle(int left, int right) {
+		int middle1 = (left + right) >> 1;
+		int middle2 = left + ((right - left) >> 1);
+		cout << "middle 1 = " << middle1 << endl;
+		cout << "middle 2 = " << middle2 << endl;
+		cout << endl;
+
+		if (middle1 != middle2)
+			return -1;
+      
+		return 0;
+	}
+  
+};
+
+int main()
+{
+	Solution A;  
+	for (int i = 0; i < 1000; ++i) {
+		cout << "number is: " << i << endl;
+		if (A.getMiddle(rand(), rand()))
+			break;
+	}
+  
+	return 0;
+}
